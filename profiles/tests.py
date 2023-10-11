6998b02aaa54fb272ac61cb0fbcc56790e4bff15
Thao <thao.maiphuong@outlook.fr>
2023-10-11 13:45:07 +0200

    add test view for list and coverage test

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,20 @@
+"""
+Test module
+"""
 from django.test import TestCase
+import pytest
+from django.contrib.auth.models import User
+from pytest_django.asserts import assertTemplateUsed
+from django.urls import reverse, resolve
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_profiles_list_view(client):
+    """
+    Test desplay profiles
+    return code 200
+    """
+    response = client.get('/profiles/')
+    assert response.status_code == 200
+    assert "Profiles" in response.content.decode()
+
+
