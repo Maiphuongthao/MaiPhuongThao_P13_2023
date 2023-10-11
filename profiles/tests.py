d8ce2503942f358871b0487073ac0d88a2d9ddf5
Thao <thao.maiphuong@outlook.fr>
2023-10-11 16:07:32 +0200

    add test for detail profile view

--- profiles/tests.py
+++ profiles/tests.py
@@ -6,6 +6,7 @@ import pytest
 from django.contrib.auth.models import User
 from pytest_django.asserts import assertTemplateUsed
 from django.urls import reverse, resolve
+from .models import Profile
 
 @pytest.mark.django_db
 def test_profiles_list_view(client):
@@ -18,3 +19,23 @@ def test_profiles_list_view(client):
     assert "Profiles" in response.content.decode()
 
 
+@pytest.mark.django_db
+def test_detail_profile_view(client):
+    """
+    Test detail of a profile
+    status code 2000
+    expect content
+    """
+    user = User.objects.create_user('test')
+    Profile.objects.create(
+        user=user,
+        favorite_city="Hanoi",
+    )
+    breakpoint()
+    path = reverse("profile",  kwargs={'username': "test"})
+
+    response = client.get(path)
+    expected_content = "test"
+    assert response.status_code==200
+    assert expected_content in response.content.decode()
+    assertTemplateUsed(response, "profile.html")
