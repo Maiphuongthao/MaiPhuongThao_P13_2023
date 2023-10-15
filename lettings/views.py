f1eac1f6811693e3ad3ea60ef5929e480c1e5cd6
Thao <thao.maiphuong@outlook.fr>
2023-10-15 21:30:34 +0200

    check erreurs following docs

--- lettings/views.py
+++ lettings/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Letting
 
 
-def lettings_index(request):
+def index(request):
     """
     Show list of lettings
     :param request from user
@@ -10,7 +10,7 @@ def lettings_index(request):
     """
     lettings_list = Letting.objects.all()
     context = {"lettings_list": lettings_list}
-    return render(request, "lettings_index.html", context)
+    return render(request, "lettings/index.html", context)
 
 
 def letting(request, letting_id):
@@ -24,4 +24,4 @@ def letting(request, letting_id):
         "title": letting.title,
         "address": letting.address,
     }
-    return render(request, "letting.html", context)
+    return render(request, "lettings/letting.html", context)
