355235479131cf478f073097b6eb4497de4c134f
Thao <thao.maiphuong@outlook.fr>
2023-10-18 13:48:38 +0200

    fix image

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -16,7 +16,7 @@ SENTRY_DNS = os.getenv("SENTRY_DNS")
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
