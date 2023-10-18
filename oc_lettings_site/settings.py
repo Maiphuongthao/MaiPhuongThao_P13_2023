15c43f7d8f7383a7b559c567720a7cf264796760
Thao <thao.maiphuong@outlook.fr>
2023-10-18 16:08:16 +0200

    fix image

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -120,7 +120,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' 
 
 # Sentry setup
 sentry_sdk.init(
