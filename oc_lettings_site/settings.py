64536e9e1396d8f138fab97b1a8c1a1855d0dfea
Thao <thao.maiphuong@outlook.fr>
2023-10-19 22:32:24 +0200

    test static

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = "/static/"
 # radically staticfiles = self-container unit can be deployed anywhere
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-# STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
 
 # Sentry setup
 sentry_sdk.init(
